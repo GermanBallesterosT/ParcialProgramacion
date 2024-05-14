0e84bbe341a53205b5f62a017630dcd79bc4a01f
guille2343 <132414127+guille2343@users.noreply.github.com>
2024-05-13 19:15:12 -0500

    Agregar archivos de proyecto.

---
+++ ConsoleApp1/Producto.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    public class Producto
+    {
+        public int referencia { get; set; }
+        public string nombre { get; set; }
+        public int existencias { get; set; }
+        public int stockMinimo { get; set; }
+        public decimal precioUnitario { get; set; }
+        public bool estado { get; set; }
+    }
+}
